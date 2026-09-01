ecbc433f1dd3b71e9807e7db76c0e9da276e4af2
Yuting(Joanna) <yuting.wan0427@gmail.com>
2026-09-01 14:37:35 -0700

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = []
+        nums.sort()
+
+        def dfs(start):
+            #每一层都是一个合法的子集
+            res.append(path[:])
+
+            for j in range(start,len(nums)):
+                #j > start去除同一层的重复
+                if j > start and nums[j] == nums[j-1]:
+                    continue
+                path.append(nums[j])
+                dfs(j+1)
+                path.pop()
+                
+            
+        dfs(0)
+        return res
+
+                
+            
