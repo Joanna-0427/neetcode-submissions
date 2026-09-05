608d0af40b578bcf04a27310f997e53b572b0361
Yuting(Joanna) <yuting.wan0427@gmail.com>
2026-09-05 16:20:45 -0700

    Add: matchsticks-to-square - submission-4

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total = sum(matchsticks)
+        if total % 4:
+            return False
+        target = total // 4
+        used = [False] * len(matchsticks)
+        matchsticks.sort(reverse=True)
+
+        def dfs(i,k,subsetsum):
+            if k == 0:
+                return True
+            
+            if subsetsum == target:
+                return dfs(0,k-1,0)
+            
+            for j in range(i,len(matchsticks)):
+                if used[j] or subsetsum + matchsticks[j] > target:
+                    continue
+                
+                used[j] = True
+                if dfs(j+1,k,subsetsum+matchsticks[j]):
+                    return True
+                used[j] = False
+            
+            return False
+        
+        return dfs(0,4,0)
