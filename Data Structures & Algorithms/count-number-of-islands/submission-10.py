bed43c84d70ef6a2b6ca141c5c5650e15420055b
Yuting(Joanna) <yuting.wan0427@gmail.com>
2026-09-06 12:45:19 -0700

    Add: count-number-of-islands - submission-10

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-10.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid),len(grid[0])
+        visited = set()
+
+        def dfs(i,j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == '0' or (i,j) in visited:
+                return
+            
+            #记数方式：内层记数，外层直接相加dfs的返回值
+            visited.add((i,j))
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            return 1
+        
+        total = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and (i,j) not in visited:
+                    total += dfs(i,j)
+        return total
+
