ae919fa4e1b61a4bf5a6036b88f1486d62290e23
Yuting(Joanna) <yuting.wan0427@gmail.com>
2026-09-12 14:04:11 -0700

    Add: accounts-merge - submission-1

---
+++ Data Structures & Algorithms/accounts-merge/submission-1.py
@@ -0,0 +1,55 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        par = {}
+        #每个邮箱的父节点都是自己
+        for account in accounts:
+            for email in account[1:]:
+                par[email] = email
+
+        def find(x):
+            while x != par[x]:
+                x = par[x]
+            return par[x]
+
+        def union(x,y):
+            root1, root2 = find(x), find(y)
+            par[root1] = root2
+
+        #每组邮箱都建立父子关系，a-b,b-c,c-c
+        for account in accounts:
+            first_email = account[1]
+            for email in account[2:]:
+                union(first_email,email)
+        
+        #找到根节点c，把一组合并
+        groups = defaultdict(list)
+        for email in par:
+            root = find(email)
+            groups[root].append(email)
+
+        #记录每个邮箱对应的姓名
+        email_to_name = {}
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                email_to_name[email] = name
+        
+        #root对应的name，对应的一组group
+        res = []
+        for root, emails in groups.items():
+            name = email_to_name[root]
+            res.append([name]+sorted(emails)) #list+list合并
+
+        return res
+
+
+
+        
+
+        
+        
+
+            
+            
+
+            
